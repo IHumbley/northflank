dff5ba824e03eeb006dabd554cb810bcf7c4b57d
Humbley <54212575+IHumbley@users.noreply.github.com>
2026-05-22 03:23:33 +0330

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: "Arial",
+      textAlign: "center",
+      marginTop: "80px"
+    }}>
+      <h1>🚀 Relay Server</h1>
+      <p>System is running...</p>
+      <p>API Path: /api/relay</p>
+    </div>
+  );
+}
